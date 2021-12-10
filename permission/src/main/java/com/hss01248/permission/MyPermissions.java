2d3b6f1f8b7615baccc437ff6efd35b4fa031c96
hss01248 <hss012489@gmail.com>
2021-12-10 18:04:49 +0800

    多权限请求适配

--- permission/src/main/java/com/hss01248/permission/MyPermissions.java
+++ permission/src/main/java/com/hss01248/permission/MyPermissions.java
@@ -7,6 +7,7 @@ import androidx.annotation.Nullable;
 
 import com.blankj.utilcode.util.ActivityUtils;
 import com.blankj.utilcode.util.IntentUtils;
+import com.blankj.utilcode.util.LogUtils;
 import com.blankj.utilcode.util.PermissionUtils;
 import com.blankj.utilcode.util.Utils;
 import com.hss01248.activityresult.ActivityResultListener;
@@ -17,8 +18,12 @@ import java.util.List;
 
 public class MyPermissions {
 
+
+
     public static void requestByMostEffort( IPermissionDialog dialogBeforeRequest,
-                                           IPermissionDialog dialogAfterDenied,PermissionUtils.FullCallback callback,String... permission){
+                                           IPermissionDialog dialogAfterDenied,
+                                            PermissionUtils.FullCallback callback,
+                                            String... permission){
         List<String> permissionsList = new ArrayList<>();
         for (String s : permission) {
             permissionsList.add(s);
@@ -31,7 +36,7 @@ public class MyPermissions {
             dialogBeforeRequest.show(permissionsList,new IPermissionDialogBtnClickListener() {
                 @Override
                 public void onPositive() {
-                    requestPermission(true,dialogAfterDenied,true,callback,permission);
+                    requestPermission(true,dialogAfterDenied,true,callback,permissionsList,permission);
                 }
 
                 @Override
@@ -40,7 +45,7 @@ public class MyPermissions {
                 }
             });
         }else {
-            requestPermission(false,dialogAfterDenied,true,callback,permission);
+            requestPermission(false,dialogAfterDenied,true,callback,permissionsList,permission);
         }
 
 
@@ -51,133 +56,168 @@ public class MyPermissions {
     private static void requestPermission(
                                           boolean hasShowBeforeDialog,
                                           IPermissionDialog dialogAfterDenied,
-                                          boolean needRetry,
-                                          PermissionUtils.FullCallback callback,String... permission){
+                                          boolean isFirstTimeRequest,
+                                          PermissionUtils.FullCallback callback,
+                                          List<String> permissionsList,
+                                          String... permission){
         //如何知道此处有系统弹窗?
         long sysPermissionDialogShowTime = System.currentTimeMillis();
         PermissionUtils.permission(permission)
-             /*   .rationale(new PermissionUtils.OnRationaleListener() {
-                    @Override
-                    public void rationale(@NonNull UtilsTransActivity activity, @NonNull ShouldRequest shouldRequest) {
-                        new AlertDialog.Builder(ActivityUtils.getTopActivity())
-                                .setTitle("rationale")
-                                .setMessage("rationale 需要xx权限来继续:\n"+ permission)
-                                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        shouldRequest.again(true);
-
-                                    }
-                                }).setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                                callbackIfDenied(permission,false,callback);
-
-                            }
-                        }).show();
-                    }
-                })*/
                 .callback(new PermissionUtils.FullCallback() {
                     @Override
                     public void onGranted(@NonNull List<String> granted) {
+                        LogUtils.d("onGranted",granted);
                         callback.onGranted(granted);
                     }
                     @Override
                     public void onDenied(@NonNull List<String> deniedForever, @NonNull List<String> denied) {
-                        if(needRetry){
+                        LogUtils.d("onDenied",deniedForever,denied);
+                        if(!isFirstTimeRequest){
+                            callback.onDenied(deniedForever, denied);
+                        }else {
                             long duration = System.currentTimeMillis() - sysPermissionDialogShowTime;
                             boolean hasShowSysDialog = duration > 1000;
                             //小于1s,说明没有显示系统权限弹窗,直接调用了denied
-                            if(deniedForever.contains(permission)){
-                                showPermissionDialog(hasShowBeforeDialog,hasShowSysDialog,dialogAfterDenied,true,callback,permission);
-                            }else if(denied.contains(permission)){
-                                showPermissionDialog(hasShowBeforeDialog,hasShowSysDialog,dialogAfterDenied,false,callback,permission);
-                            }
-                        }else {
-                            callback.onDenied(deniedForever, denied);
+                            checkIfShowAfterDeniedDialog(hasShowBeforeDialog,hasShowSysDialog, true,
+                                    dialogAfterDenied,deniedForever,denied,callback,permissionsList,permission);
+
                         }
                     }
                 }).request();
     }
 
-    private static void showPermissionDialog(
+    private static void checkIfShowAfterDeniedDialog(
                                              boolean hasShowBeforeDialog,
                                              boolean hasShowSysDialog,
+                                             boolean isFirstTimeResult,
                                              IPermissionDialog dialogAfterDenied,
-                                             boolean deniedForever,
-                                             PermissionUtils.FullCallback callback,String... permission) {
-        if(dialogAfterDenied == null){
-            if(deniedForever){
-                requestGoSettings( callback,permission);
+                                             List<String> deniedForever,
+                                             List<String> denied,
+                                             PermissionUtils.FullCallback callback,
+                                             List<String> permissionsList,
+                                             String... permission) {
+        //当没有显示系统权限弹窗,     则永久拒绝肯定不为空,普通拒绝肯定为空
+        if( !hasShowSysDialog ){
+            //&& !deniedForever.isEmpty()
+            LogUtils.i("check !deniedForever.isEmpty()  && denied.isEmpty()");
+            //如果之前显示了before的dialog,那么就跳去系统设置页
+            //或者没有设置dialog after,则
+            if(hasShowBeforeDialog){
+                requestGoSettings(hasShowBeforeDialog,hasShowSysDialog,isFirstTimeResult,
+                        dialogAfterDenied,deniedForever,denied,callback,permissionsList,permission);
             }else {
-                callbackIfDenied( deniedForever, callback,permission);
+                if(dialogAfterDenied == null){
+                    requestGoSettings(hasShowBeforeDialog,hasShowSysDialog,isFirstTimeResult,
+                            dialogAfterDenied,deniedForever,denied,callback,permissionsList,permission);
+                }else {
+                    dialogAfterDenied.show(permissionsList,new IPermissionDialogBtnClickListener() {
+                        @Override
+                        public void onPositive() {
+                            requestGoSettings(hasShowBeforeDialog,hasShowSysDialog,isFirstTimeResult,
+                                    dialogAfterDenied,deniedForever,denied,callback,permissionsList,permission);
+                        }
+
+                        @Override
+                        public void onNegtivite() {
+                            callback.onDenied(deniedForever, denied);
+                        }
+                    });
+                }
+
+
             }
-            return;
+        }
+        if(denied.isEmpty() && !deniedForever.isEmpty()){
+            //弹出了系统权限弹窗,但被用户点击了永久拒绝,那么要挽回一下:
+            if(dialogAfterDenied == null){
+                //没有配置挽回弹窗,那就不挽回
+                callback.onDenied(deniedForever,denied);
+            }else {
+                //如果是重试,则不再弹窗
+                if(!isFirstTimeResult){
+                    callback.onDenied(deniedForever,denied);
+                }else {
+                    showAfterDeniedDialog(hasShowBeforeDialog,hasShowSysDialog,isFirstTimeResult,
+                            dialogAfterDenied,deniedForever,denied,callback,permissionsList,permission);
+                }
+            }
+
+        }else if(!denied.isEmpty() && deniedForever.isEmpty()){
+            //全部都是普通拒绝,没有永久拒绝,则再次申请权限即可
+            if(dialogAfterDenied == null){
+                //没有配置挽回弹窗,那就不挽回
+                callback.onDenied(deniedForever,denied);
+            }else {
+                //如果是重试,则不再弹窗
+                if(!isFirstTimeResult){
+                    callback.onDenied(deniedForever,denied);
+                }else {
+                    showAfterDeniedDialog(hasShowBeforeDialog,hasShowSysDialog,isFirstTimeResult,
+                            dialogAfterDenied,deniedForever,denied,callback,permissionsList,permission);
+                }
+            }
+        }else if(!denied.isEmpty() && !deniedForever.isEmpty()){
+            //两者都有,那么
         }
 
-        //此处判断有问题,无法指定beforedialog_->系统权限弹窗->after弹窗  直接是否有系统权限弹窗.
-        if(hasShowBeforeDialog && !hasShowSysDialog && deniedForever){
-            requestGoSettings(callback,permission);
-            return;
-        }
-        List<String> permissions = new ArrayList<>();
-        for (String s : permission) {
-            permissions.add(s);
-        }
-        dialogAfterDenied.show(permissions,new IPermissionDialogBtnClickListener() {
+
+
+
+    }
+
+    private static void showAfterDeniedDialog(boolean hasShowBeforeDialog,
+                                              boolean hasShowSysDialog,
+                                              boolean isCurrentRetryResult,
+                                              IPermissionDialog dialogAfterDenied,
+                                              List<String> deniedForever,
+                                              List<String> denied,
+                                              PermissionUtils.FullCallback callback,
+                                              List<String> permissionsList,
+                                              String[] permission) {
+
+        dialogAfterDenied.show(permissionsList,new IPermissionDialogBtnClickListener() {
             @Override
             public void onPositive() {
-                if(deniedForever){
-                    requestGoSettings( callback,permission);
-                }else {
-                    requestPermission(true,dialogAfterDenied, deniedForever, callback,permission);
-                }
+
             }
 
             @Override
             public void onNegtivite() {
-                callbackIfDenied( deniedForever, callback,permission);
+
             }
         });
+
     }
 
-    private static void callbackIfDenied(boolean deniedForever, PermissionUtils.FullCallback callback,String... permission) {
-        List<String> permissions = new ArrayList<>();
-        for (String s : permission) {
-            permissions.add(s);
-        }
-        if (deniedForever) {
-            callback.onDenied(permissions, new ArrayList<>());
-        } else {
-            callback.onDenied(new ArrayList<>(), permissions);
-        }
-    }
-
-    private static void requestGoSettings(PermissionUtils.FullCallback callback,String... permission) {
+    private static void requestGoSettings(boolean hasShowBeforeDialog,
+                                          boolean hasShowSysDialog,
+                                          boolean isFirstTimeUIRequest,
+                                          IPermissionDialog dialogAfterDenied,
+                                          List<String> deniedForever,
+                                          List<String> denied,
+                                          PermissionUtils.FullCallback callback,
+                                          List<String> permissionsList,
+                                          String... permission) {
         Intent intent = IntentUtils.getLaunchAppDetailsSettingsIntent(Utils.getApp().getPackageName(), false);
         StartActivityUtil.goOutAppForResult(ActivityUtils.getTopActivity(), intent, new ActivityResultListener() {
             @Override
             public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-                List<String> permissions = new ArrayList<>();
-                for (String s : permission) {
-                    permissions.add(s);
-                }
                 if (PermissionUtils.isGranted(permission)) {
-                    callback.onGranted(permissions);
+                    callback.onGranted(permissionsList);
                 } else {
-                    callback.onDenied(permissions, new ArrayList<>());
+                    if(isCurrentRetryResult){
+                        callback.onDenied(permissionsList, new ArrayList<>());
+                    }else {
+                        if(hasShowBeforeDialog);
+                    }
+
                 }
             }
 
             @Override
             public void onActivityNotFound(Throwable e) {
                 e.printStackTrace();
-                List<String> permissions = new ArrayList<>();
-                for (String s : permission) {
-                    permissions.add(s);
-                }
-                callback.onDenied(permissions, new ArrayList<>());
+                callback.onDenied(permissionsList, new ArrayList<>());
             }
         });
     }
