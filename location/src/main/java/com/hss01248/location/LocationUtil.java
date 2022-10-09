553adf579c42e20539672f4c916f30da242c8516
hss01248 <hss012489@gmail.com>
2022-10-09 17:59:38 +0800

    添加注释

--- location/src/main/java/com/hss01248/location/LocationUtil.java
+++ location/src/main/java/com/hss01248/location/LocationUtil.java
@@ -286,8 +286,8 @@ public class LocationUtil {
     }
 
     private static void checkPermission(Context context, int timeout, boolean showBeforeRequest, boolean showAfterRequest,boolean withoutGms, MyLocationCallback callback) {
-        if (PermissionUtils.isGranted(Manifest.permission.ACCESS_COARSE_LOCATION )
-             && PermissionUtils.isGranted(Manifest.permission.ACCESS_FINE_LOCATION)){
+        if ( PermissionUtils.isGranted(Manifest.permission.ACCESS_FINE_LOCATION) &&
+            PermissionUtils.isGranted(Manifest.permission.ACCESS_COARSE_LOCATION )){
             doRequestLocation(context, timeout,withoutGms, callback);
         } else {
             MyPermissions.requestByMostEffort(
@@ -304,7 +304,9 @@ public class LocationUtil {
                             callback.onFailed(1, "no permission",true);
                         }
                     }, Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION);
-            //Android 12 ACCESS_COARSE_LOCATION 模糊定位,accuracy=2000米, 中心点随机偏差几千米
+            //
+            //只有ACCESS_COARSE_LOCATION时,  模糊定位,accuracy=2000米, 中心点随机偏差几千米
+            //只有ACCESS_FINE_LOCATION时,精确定位 android12也会跳出模糊定位的选项.
         }
     }
 
