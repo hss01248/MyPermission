621ca779ba963f8bba666aa1ecc26d9d8c9eb362
hss01248 <hss012489@gmail.com>
2023-07-14 17:32:12 +0800

    fix(bug): dialog延迟200ms避免token问题

--- permission/src/main/java/com/hss01248/permission/DefaultAlertDialogImpl.java
+++ permission/src/main/java/com/hss01248/permission/DefaultAlertDialogImpl.java
@@ -7,6 +7,7 @@ import android.text.TextUtils;
 import androidx.appcompat.app.AlertDialog;
 
 import com.blankj.utilcode.util.ActivityUtils;
+import com.blankj.utilcode.util.ThreadUtils;
 
 
 /**
@@ -23,38 +24,45 @@ public class DefaultAlertDialogImpl implements IAlertDialog{
                             DialogInterface.OnClickListener positiveOnClick,
                             DialogInterface.OnClickListener negativeOnClick
                            ) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(ActivityUtils.getTopActivity());
-        if(!TextUtils.isEmpty(title)){
-            builder.setTitle(title);
-        }
-        if(!TextUtils.isEmpty(msg)){
-            builder.setMessage(msg);
-        }
-        if(!TextUtils.isEmpty(positiveText)){
-            builder.setPositiveButton(positiveText, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    if(positiveOnClick !=null){
-                        positiveOnClick.onClick(dialog, which);
-                    }
+        //为防止activity调用了finish(),但还没有将isFinishing标识改成true的情况,这里延时200ms:
+        ThreadUtils.getMainHandler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ActivityUtils.getTopActivity());
+                if(!TextUtils.isEmpty(title)){
+                    builder.setTitle(title);
                 }
-            });
-        }
-        if(!TextUtils.isEmpty(negativeText)){
-            builder.setNegativeButton(negativeText, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    if(negativeOnClick !=null){
-                        negativeOnClick.onClick(dialog, which);
-                    }
+                if(!TextUtils.isEmpty(msg)){
+                    builder.setMessage(msg);
                 }
-            });
-        }
-        AlertDialog alertDialog = builder.create();
-        alertDialog.setCanceledOnTouchOutside(outsideCancelable);
-        alertDialog.setCancelable(cancelable);
-        alertDialog.show();
+                if(!TextUtils.isEmpty(positiveText)){
+                    builder.setPositiveButton(positiveText, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            if(positiveOnClick !=null){
+                                positiveOnClick.onClick(dialog, which);
+                            }
+                        }
+                    });
+                }
+                if(!TextUtils.isEmpty(negativeText)){
+                    builder.setNegativeButton(negativeText, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            if(negativeOnClick !=null){
+                                negativeOnClick.onClick(dialog, which);
+                            }
+                        }
+                    });
+                }
+                AlertDialog alertDialog = builder.create();
+                alertDialog.setCanceledOnTouchOutside(outsideCancelable);
+                alertDialog.setCancelable(cancelable);
+                alertDialog.show();
+            }
+        },200);
 
-        return alertDialog;
+
+        return null;
     }
 }
