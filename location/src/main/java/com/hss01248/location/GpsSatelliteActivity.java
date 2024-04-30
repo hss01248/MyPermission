1cc35dce891a0490c1fffcf34289a13dd2cf4a41
hss01248 <hss012489@gmail.com>
2024-04-30 16:25:54 +0800

    feat(ext): 卫星信息展示页面

--- location/src/main/java/com/hss01248/location/GpsSatelliteActivity.java
+++ location/src/main/java/com/hss01248/location/GpsSatelliteActivity.java
@@ -21,11 +21,15 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+
 import com.blankj.utilcode.util.ActivityUtils;
 import com.blankj.utilcode.util.LogUtils;
 import com.blankj.utilcode.util.ToastUtils;
 import com.hss01248.location.databinding.GpsStatusInfoBinding;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -141,6 +145,8 @@ public class GpsSatelliteActivity extends AppCompatActivity {
         StringBuilder builder = new StringBuilder();
         builder.append("Satellite count: ");
         builder.append(status.getSatelliteCount());
+        //先排序:
+        List<Map<String,Object>> list = new ArrayList<>();
 
         for (int i = 0; i < status.getSatelliteCount(); i++) {
             builder.append("\n\n");
@@ -174,7 +180,32 @@ public class GpsSatelliteActivity extends AppCompatActivity {
         }
         String text = builder.toString();
         binding.tvSatellitesInfo.setText(text);
+
+        //showInTable(status);
     }
+
+/*    private void showInTable(GnssStatus status) {
+        //普通列
+        Column<String> column1 = new Column<>("Svid", "Svid");
+        Column<Integer> column2 = new Column<>("AzimuthDegrees", "AzimuthDegrees");
+        Column<Long> column3 = new Column<>("ElevationDegrees", "ElevationDegrees");
+        Column<String> column4 = new Column<>("ConstellationType", "ConstellationType");
+        Column<String> column5 = new Column<>("ConstellationType", "ConstellationType");
+        Column<String> column6 = new Column<>("BasebandCn0DbHz", "BasebandCn0DbHz");
+        Column<String> column7 = new Column<>("CarrierFrequencyHz", "CarrierFrequencyHz");
+
+        List<Map<String,Object>> list = new ArrayList<>();
+        //如果是多层，可以通过.来实现多级查询
+       // Column<String> column5 = new Column<>("班级", "class.className");
+        //组合列
+        //Column totalColumn1 = new Column("组合列名",column1,column2);
+        //表格数据 datas是需要填充的数据
+        final TableData<Map<String,Object>> tableData = new TableData<>("卫星数据",list,
+                column1, column2,column3,column4,column5,column6,column7);
+        //设置数据
+        //table.setZoom(true,3);是否缩放
+        binding.table.setTableData(tableData);
+    }*/
 
     private void showLocationInfo(Location location) {
         String text = "点击可跳到地图查看\n"+locationToString(location);
@@ -193,6 +224,7 @@ public class GpsSatelliteActivity extends AppCompatActivity {
         }
         StringBuilder sb = new StringBuilder();
         sb.append(location.toString()).append("\n");
+        sb.append("Provider: ").append(location.getProvider()).append("\n");
         sb.append("Latitude: ").append(location.getLatitude()).append("\n");
         sb.append("Longitude: ").append(location.getLongitude()).append("\n");
         sb.append("Altitude: ").append(location.getAltitude()).append("\n");
@@ -200,6 +232,22 @@ public class GpsSatelliteActivity extends AppCompatActivity {
         sb.append("Bearing: ").append(location.getBearing()).append("\n");
         sb.append("Time: ").append(location.getTime()).append("\n");
         sb.append("Accuracy: ").append(location.getAccuracy()).append("\n");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            sb.append("ElapsedRealtimeNanos: ").append(location.getElapsedRealtimeNanos()).append("\n");
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            sb.append("ElapsedRealtimeUncertaintyNanos: ").append(location.getElapsedRealtimeUncertaintyNanos()).append("\n");
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            sb.append("VerticalAccuracyMeters: ").append(location.getVerticalAccuracyMeters()).append("\n");
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            sb.append("SpeedAccuracyMetersPerSecond: ").append(location.getSpeedAccuracyMetersPerSecond()).append("\n");
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            sb.append("BearingAccuracyDegrees: ").append(location.getBearingAccuracyDegrees()).append("\n");
+        }
+
         if(location.getExtras() !=null){
             sb.append("extras-------->: size:").append(location.getExtras().size()).append("\n");
             Set<String> stringSet = location.getExtras().keySet();
